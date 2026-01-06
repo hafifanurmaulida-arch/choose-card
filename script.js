0fe287ca9622e274acc38b0136b67d825dd506bc
hafifanurmaulida-arch <hafifa.nurmaulida@gmail.com>
2026-01-07 00:59:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 function openCard(card) {
-  if (card.classList.contains("open")) return;
-
   document.querySelectorAll(".envelope").forEach(el => {
     el.classList.remove("open");
   });
 
   card.classList.add("open");
-
-  const sound = document.getElementById("sound");
-  sound.currentTime = 0;
-  sound.play();
 }
